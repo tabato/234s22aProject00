43a0a3c3dc3a04f93fe93847b9cfd3726c0badd7
JakeClause <99262232+JakeClause@users.noreply.github.com>
2022-02-08 09:40:48 -0500

    Update TeamSix.java

--- 234s22aProject00/src/TeamSix.java
+++ 234s22aProject00/src/TeamSix.java
@@ -9,6 +9,7 @@ public class TeamSix extends Team
     public void createTeamMembers()
     {
         members.add(new TeamMember("Thomas Abato"));
+        members.add(new TeamMember("Jake Clause"));
         // create a new member class for each team member
         // and add it to the team members list
     }
