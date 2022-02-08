dfcbcf640f5babe743fd564f0c522c5527d8a111
lukesuppa <99268105+lukesuppa@users.noreply.github.com>
2022-02-08 09:44:04 -0500

    Update TeamSix.java

--- 234s22aProject00/src/TeamSix.java
+++ 234s22aProject00/src/TeamSix.java
@@ -11,6 +11,7 @@ public class TeamSix extends Team
         members.add(new TeamMember("Thomas Abato"));
         members.add(new TeamMember("Jake Clause"));
         members.add(new TeamMember("Colin Conway"));
+        members.add(new TeamMember("Luke Suppa"));
         // create a new member class for each team member
         // and add it to the team members list
     }
