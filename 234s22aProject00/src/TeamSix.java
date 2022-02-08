ecc8792ee1f7663961528e92d9928be1e8f59d23
tabato <99261656+tabato@users.noreply.github.com>
2022-02-08 09:38:38 -0500

    Update TeamSix.java

--- 234s22aProject00/src/TeamSix.java
+++ 234s22aProject00/src/TeamSix.java
@@ -1,2 +1,15 @@
-Thomas Abato
-Jake Clause
+public class TeamSix extends Team
+{
+    public TeamSix(String name)
+    {
+        super(name);
+        createTeamMembers();
+    }
+
+    public void createTeamMembers()
+    {
+        members.add(new TeamMember("Thomas Abato"));
+        // create a new member class for each team member
+        // and add it to the team members list
+    }
+}
